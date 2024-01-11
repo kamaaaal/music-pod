9e55cb662e0a2d97efaebc718d704eaeab49a973
kamaaaal <itskamalmohamed@gmail.com>
2024-01-11 23:11:04 +0530

    feat: added a basic login form

--- apps/client/src/pages/login.tsx
+++ apps/client/src/pages/login.tsx
@@ -1,47 +1,112 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/Form";
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Form, FormControl, FormField } from "@/components/ui/Form";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 /**
  * Home or login page
  */
-export function Login() {
-  const form = useForm();
-  console.log(form.formState);
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(5, "User Name should be greater than 5 chars")
+    .max(15, "User Name cannot be more than 15 chars"),
+  password: z.string().min(6, "Password cannot be less than 6 chars"),
+});
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { username: "", password: "" },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <Form {...form}>
-      <FormField
-        control={form.control}
-        name="UserName"
-        render={() => (
-          <FormItem>
-            <FormLabel />
-            <FormControl></FormControl>
-            <FormDescription />
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="password"
-        render={() => (
-          <FormItem>
-            <FormLabel title="Password" />
-            <FormControl></FormControl>
-            <FormDescription />
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </Form>
+    <Card className="w-[350px] glass-back">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
+      </CardHeader>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <Form {...form}>
+              <FormField
+                name={"username"}
+                render={(fieldProps) => {
+                  const error = fieldProps.fieldState?.error?.message;
+                  return (
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">{fieldProps.field.name}</Label>
+                      <FormControl>
+                        <Input
+                          id="name"
+                          placeholder="username"
+                          {...fieldProps.field}
+                        />
+                      </FormControl>
+                      <p
+                        className={`transition-transform text-sm font-medium text-destructive text-center ${
+                          error ? "scale-100" : "scale-0"
+                        }`}
+                      >
+                        {" "}
+                        {error}
+                      </p>
+                    </div>
+                  );
+                }}
+              ></FormField>
+
+              <FormField
+                name={"password"}
+                render={(fieldProps) => {
+                  const error = fieldProps.fieldState.error?.message;
+                  return (
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="password">{fieldProps.field.name}</Label>
+                      <FormControl>
+                        <Input
+                          id="name"
+                          type="password"
+                          placeholder="password"
+                          {...fieldProps.field}
+                        />
+                      </FormControl>
+                      <p
+                        className={`transition-transform text-sm  font-medium text-destructive text-center ${
+                          error ? "scale-100" : "scale-0"
+                        }`}
+                      >
+                        {" "}
+                        {error || " t"}
+                      </p>
+                    </div>
+                  );
+                }}
+              ></FormField>
+            </Form>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button type="submit">Login</Button>
+        </CardFooter>
+      </form>
+    </Card>
   );
 }
